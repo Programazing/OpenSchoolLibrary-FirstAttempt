8124de396db9c4dfd1f8edfc79bf08b13cb73d5f
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 09:26:08 -0400

    Creats /Models/Database/Models/Setting Class.

---
+++ src/Open-School-Library/Models/DatabaseModels/Setting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.DatabaseModels
+{
+    public class Setting
+    {
+        public int SettingID { get; set; }
+        [Column(TypeName = "Money")]
+        public decimal FineAmount { get; set; }
+    }
+}
