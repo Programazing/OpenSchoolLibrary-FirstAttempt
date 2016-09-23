994ff5a6f352e5dd0432eab46db926420261d7ff
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 01:25:14 -0400

    Create /Models/DatabaseModels/Teacher Class.

---
+++ src/Open-School-Library/Models/DatabaseModels/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.DatabaseModels
+{
+    public class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+    }
+}
