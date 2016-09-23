1dbcd2d2f48458e672bffab64c1541e778b21b0d
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 09:40:30 -0400

    Create /Models/DatabaseModels/BookLoan Class.

---
+++ src/Open-School-Library/Models/DatabaseModels/BookLoan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.DatabaseModels
+{
+    public class BookLoan
+    {
+        public int BookLoanID { get; set; }
+        public int BookID { get; set; }
+        public int StudentID { get; set; }
+        public DateTime CheckedOutWhen { get; set; }
+        public DateTime DueWhen { get; set; }
+        public DateTime ReturnedWhen { get; set; }
+
+        public Book Book { get; set; }
+        public Student Student { get; set; }
+    }
+}
