c9334610300a54d72060abeef00244eb8c6184a1
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 10:29:00 -0400

    Create TeachersController along with Views/Teachers and Index, Edit, Details, Delete, and Create Views. This is all one action though Add Scaffolded Item.

---
+++ src/Open-School-Library/Controllers/TeachersController.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Open_School_Library.Data;
+using Open_School_Library.Models.DatabaseModels;
+
+namespace Open_School_Library.Controllers
+{
+    public class TeachersController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public TeachersController(LibraryContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Teachers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Teachers.ToListAsync());
+        }
+
+        // GET: Teachers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacher = await _context.Teachers.SingleOrDefaultAsync(m => m.TeacherID == id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return View(teacher);
+        }
+
+        // GET: Teachers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teachers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeacherID,FirstName,Grade,LastName")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(teacher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        // GET: Teachers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacher = await _context.Teachers.SingleOrDefaultAsync(m => m.TeacherID == id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Teachers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TeacherID,FirstName,Grade,LastName")] Teacher teacher)
+        {
+            if (id != teacher.TeacherID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(teacher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeacherExists(teacher.TeacherID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        // GET: Teachers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacher = await _context.Teachers.SingleOrDefaultAsync(m => m.TeacherID == id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return View(teacher);
+        }
+
+        // POST: Teachers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var teacher = await _context.Teachers.SingleOrDefaultAsync(m => m.TeacherID == id);
+            _context.Teachers.Remove(teacher);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return _context.Teachers.Any(e => e.TeacherID == id);
+        }
+    }
+}
