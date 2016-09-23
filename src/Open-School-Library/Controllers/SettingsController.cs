73d782b8d553bc4923ce763017955461c69e4fe7
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 10:13:36 -0400

    Create SettingsController along with Views/Settings and Index, Edit, Details, Delete, and Create Views. This is all one action though Add Scaffolded Item.

---
+++ src/Open-School-Library/Controllers/SettingsController.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Open_School_Library.Data;
+using Open_School_Library.Models.DatabaseModels;
+
+namespace Open_School_Library.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public SettingsController(LibraryContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Settings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Settings.ToListAsync());
+        }
+
+        // GET: Settings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var setting = await _context.Settings.SingleOrDefaultAsync(m => m.SettingID == id);
+            if (setting == null)
+            {
+                return NotFound();
+            }
+
+            return View(setting);
+        }
+
+        // GET: Settings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Settings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SettingID,FineAmount")] Setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(setting);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(setting);
+        }
+
+        // GET: Settings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var setting = await _context.Settings.SingleOrDefaultAsync(m => m.SettingID == id);
+            if (setting == null)
+            {
+                return NotFound();
+            }
+            return View(setting);
+        }
+
+        // POST: Settings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SettingID,FineAmount")] Setting setting)
+        {
+            if (id != setting.SettingID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(setting);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SettingExists(setting.SettingID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(setting);
+        }
+
+        // GET: Settings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var setting = await _context.Settings.SingleOrDefaultAsync(m => m.SettingID == id);
+            if (setting == null)
+            {
+                return NotFound();
+            }
+
+            return View(setting);
+        }
+
+        // POST: Settings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var setting = await _context.Settings.SingleOrDefaultAsync(m => m.SettingID == id);
+            _context.Settings.Remove(setting);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool SettingExists(int id)
+        {
+            return _context.Settings.Any(e => e.SettingID == id);
+        }
+    }
+}
