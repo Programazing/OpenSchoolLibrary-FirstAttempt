2c9711a88b95e1bc1fc3aa0d49da5ac677366b1a
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 02:31:41 -0400

    Create /Models/DatabaseModels/Book Class.

---
+++ src/Open-School-Library/Models/DatabaseModels/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.DatabaseModels
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Author { get; set; }
+        public int GenreID { get; set; }
+        public int DeweyID { get; set; }
+        public int ISBN { get; set; }
+    }
+}
