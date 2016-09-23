901ceaa113c2de45dd91481e4cc8db5915bfc9b6
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-23 00:51:52 -0400

    Create /Models/DatabaseModels/Student Class.

---
+++ src/Open-School-Library/Models/DatabaseModels/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.DatabaseModels
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+        [Column(TypeName = "Money")]
+        public decimal Fines { get; set; }
+        public int IssusedID { get; set; }
+        public string Email { get; set; }
+        public int HomeRoomTeacher { get; set; }
+
+    }
+}
