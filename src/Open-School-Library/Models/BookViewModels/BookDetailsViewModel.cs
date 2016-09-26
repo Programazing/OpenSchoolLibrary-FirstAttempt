5f7740133093cd42ab2980b89d34b9a58fecabd4
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-25 23:12:48 -0400

    Create BookDetailsViewModel.

---
+++ src/Open-School-Library/Models/BookViewModels/BookDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.BookViewModels
+{
+    public class BookDetailsViewModel
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Author { get; set; }
+        public string GenreName { get; set; }
+        public string DeweyName { get; set; }    
+        public int ISBN { get; set; }
+        public string Availability { get; set; }
+    }
+}
