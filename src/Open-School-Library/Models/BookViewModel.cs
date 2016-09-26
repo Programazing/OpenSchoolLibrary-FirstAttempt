de36274f0547baa191df31035434bc718e1b8158
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-25 22:20:00 -0400

    Add Validation to Book Edit View through the BookViewModel.

--- src/Open-School-Library/Models/BookViewModel.cs
+++ src/Open-School-Library/Models/BookViewModel.cs
@@ -2,6 +2,7 @@
 using Open_School_Library.Models.DatabaseModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,13 +11,19 @@ namespace Open_School_Library.Models
     public class BookViewModel
     {
         public int BookID { get; set; }
+
+        [Required(ErrorMessage = "Title is Required!")]
         public string Title { get; set; }
+
         public string SubTitle { get; set; }
+
+        [Required(ErrorMessage = "Author is Required!")]
         public string Author { get; set; }
         public int GenreID { get; set; }
         public SelectList GenreList { get; set; }
         public int DeweyID { get; set; }
         public SelectList DeweyList { get; set; }
+        [Required(ErrorMessage = "Author is Required!")]
         public int ISBN { get; set; }
         public string Availability { get; set; }
     }
