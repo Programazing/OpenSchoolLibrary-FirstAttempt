255a9134fc3c34f0bba6114be94e696759c03103
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 15:45:19 -0400

    Create StudentCreateViewModel

---
+++ src/Open-School-Library/Models/StudentViewModels/StudentCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.StudentViewModels
+{
+    public class StudentCreateViewModel
+    {
+    }
+}
