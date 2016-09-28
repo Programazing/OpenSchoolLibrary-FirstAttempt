851ad1e3df5f1ab37575861d7fab4bf7f5461995
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 16:28:50 -0400

    Create TeacherEditViewModel and update Teacher Edit View to use it.

---
+++ src/Open-School-Library/Models/TeacherViewModels/TeacherEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.TeacherViewModels
+{
+    public class TeacherEditViewModel
+    {
+        public int TeacherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+    }
+}
