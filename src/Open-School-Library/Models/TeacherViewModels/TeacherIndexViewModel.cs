6f76e3ebe90ba79168b2d5c570289711ba7fd433
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 16:32:14 -0400

    Update TeacherIndexViewModel and TeacherEditViewModel to use Data Annotations.

--- src/Open-School-Library/Models/TeacherViewModels/TeacherIndexViewModel.cs
+++ src/Open-School-Library/Models/TeacherViewModels/TeacherIndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace Open_School_Library.Models.TeacherViewModels
     public class TeacherIndexViewModel
     {
         public int TeacherID { get; set; }
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public int Grade { get; set; }
     }
