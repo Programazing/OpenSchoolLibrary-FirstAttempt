6794e2d91d41958a0857ead629238d4354e3a2d9
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 16:36:08 -0400

    Update Teacher Detail View to use TeacherDetailViewModel.

--- src/Open-School-Library/Models/TeacherViewModels/TeacherDetailViewModel.cs
+++ src/Open-School-Library/Models/TeacherViewModels/TeacherDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Open_School_Library.Models.TeacherViewModels
 {
     public class TeacherDetailViewModel
     {
+        public int TeacherID { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int Grade { get; set; }
     }
 }
