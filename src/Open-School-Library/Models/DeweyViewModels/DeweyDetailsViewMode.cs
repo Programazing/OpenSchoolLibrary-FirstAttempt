29692d6375b6711b29d3bb8d0f0a21a727dbf5fc
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 17:16:59 -0400

    Update DeweyDetails to use DeweyDetailsViewmodel.

--- src/Open-School-Library/Models/DeweyViewModels/DeweyDetailsViewMode.cs
+++ src/Open-School-Library/Models/DeweyViewModels/DeweyDetailsViewMode.cs
@@ -7,5 +7,8 @@ namespace Open_School_Library.Models.DeweyViewModels
 {
     public class DeweyDetailsViewMode
     {
+        public int DeweyID { get; set; }
+        public float Number { get; set; }
+        public string Name { get; set; }
     }
 }
