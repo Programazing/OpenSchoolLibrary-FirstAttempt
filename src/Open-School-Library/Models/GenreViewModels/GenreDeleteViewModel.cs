aa294505c2f5556735126e23404861a137969b0e
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 17:32:41 -0400

    Create GeneEditViewModel, GeneDetailsViewModel, GeneDeleteViewModel, and GeneCreateViewModel because they're all the same anyway yet I wanted seperate View Models for every View.

---
+++ src/Open-School-Library/Models/GenreViewModels/GenreDeleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.GenreViewModels
+{
+    public class GenreDeleteViewModel
+    {
+        public int GenreId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/Open-School-Library/Models/GenreViewModels/GenreEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.GenreViewModels
+{
+    public class GenreEditViewModel
+    {
+        public int GenreId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
