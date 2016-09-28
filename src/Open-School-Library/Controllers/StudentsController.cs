613fd56084926549d732564f2580a471dd585348
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 16:10:53 -0400

    Update Student Delete View to use StudentDeleteViewModel.

--- src/Open-School-Library/Controllers/StudentsController.cs
+++ src/Open-School-Library/Controllers/StudentsController.cs
@@ -184,7 +184,25 @@ namespace Open_School_Library.Controllers
                 return NotFound();
             }
 
-            var student = await _context.Students.SingleOrDefaultAsync(m => m.StudentID == id);
+            var student =
+            _context.Students
+            .Where(s => s.StudentID == id)
+            .Select(r => new StudentDeleteViewModel
+            {
+
+                StudentID = r.StudentID,
+                FirstName = r.FirstName,
+                LastName = r.LastName,
+                Grade = r.Grade,
+                Fines = r.Fines,
+                IssuedID = r.IssuedID,
+                Email = r.Email,
+                TeacherID = r.TeacherID,
+                TeacherFirstName = r.Teacher.FirstName,
+                TeacherLastName = r.Teacher.LastName
+
+            }).FirstOrDefault();
+
             if (student == null)
             {
                 return NotFound();
