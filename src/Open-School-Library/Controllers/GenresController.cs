3ec483e2a78ec819ef3ec5ae1b55380fce9ee5a9
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-28 17:39:13 -0400

    Update Genre Details View to use GenreDetailsViewModel.

--- src/Open-School-Library/Controllers/GenresController.cs
+++ src/Open-School-Library/Controllers/GenresController.cs
@@ -41,7 +41,16 @@ namespace Open_School_Library.Controllers
                 return NotFound();
             }
 
-            var genre = await _context.Genres.SingleOrDefaultAsync(m => m.GenreId == id);
+            var genre =
+            _context.Genres
+            .Where(g => g.GenreId == id)
+            .Select(r => new GenreDetailsViewModel
+            {
+                GenreId = r.GenreId,
+                Name = r.Name
+
+            }).FirstOrDefault();
+
             if (genre == null)
             {
                 return NotFound();
