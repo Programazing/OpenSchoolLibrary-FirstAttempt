76d2eff1ab079c41311e6179e037b80f31b6f040
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-30 19:25:17 -0400

    Create BookIndexViewModel.

---
+++ src/Open-School-Library/Models/BookViewModels/BookIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.BookViewModels
+{
+    public class BookIndexViewModel
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Subtitle")]
+        public string Author { get; set; }
+        [Display(Name = "Genre")]
+        public string GenreName { get; set; }
+        [Display(Name = "Dewey")]
+        public string DeweyName { get; set; }
+        public int ISBN { get; set; }
+        public string Availability { get; set; }
+    }
+}
