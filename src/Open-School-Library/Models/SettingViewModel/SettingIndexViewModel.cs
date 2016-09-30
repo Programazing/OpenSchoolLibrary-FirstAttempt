675eea14ab585c6d6ff60b63caafd0915bb7da2b
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-29 22:45:42 -0400

    Update Settings Index View to use SettingIndexViewModel.

--- src/Open-School-Library/Models/SettingViewModel/SettingIndexViewModel.cs
+++ src/Open-School-Library/Models/SettingViewModel/SettingIndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Open_School_Library.Models.SettingViewModel
 {
     public class SettingIndexViewModel
     {
+        public int SettingID { get; set; }
+        [Display(Name ="Fine Ammount Per Day")]
+
+        public decimal? FineAmountPerDay { get; set; }
+        [Display(Name = "Checkout Duration in Days")]
+        public int CheckoutDurationInDays { get; set; }
     }
 }
