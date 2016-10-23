46468a50d35e7fd8d83b96ff96aaae50732889e3
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-10-23 00:56:04 -0400

    Add areFinesEnabled method to FineCalculations class.

--- src/Open-School-Library/Helpers/FineCalculations.cs
+++ src/Open-School-Library/Helpers/FineCalculations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Open_School_Library.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,10 +8,24 @@ namespace Open_School_Library.Helpers
 {
     public class FineCalculations
     {
+        private static LibraryContext _context;
+
+        public FineCalculations(LibraryContext context)
+        {
+            _context = context;
+        }
+
         public static double CalculateFine(DateTime CheckedOutOn, DateTime ReturnedOn)
         {
 
             return 90;
+        }
+
+        public static bool areFinesEnabled()
+        {
+            bool checkFines = _context.Settings.Select(x => x.AreFinesEnabled).FirstOrDefault();
+
+            return checkFines;
         }
     }
 }
