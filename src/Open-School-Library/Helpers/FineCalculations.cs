ad89ffeb40b0ccdd26b338221a52b88cfb958c2f
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-10-22 23:41:10 -0400

    Create Helpers folder and add FineCalculations class.

---
+++ src/Open-School-Library/Helpers/FineCalculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Helpers
+{
+    public class FineCalculations
+    {
+        public static double CalculateFine(DateTime CheckedOutOn, DateTime ReturnedOn)
+        {
+
+            return 90;
+        }
+    }
+}
