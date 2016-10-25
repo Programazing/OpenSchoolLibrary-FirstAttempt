5371fe2256ed566d5ecc5e2b08deab803ca3f4bb
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-10-25 16:08:45 -0400

    Add Migrations.

---
+++ src/Open-School-Library/Migrations/20161025194139_InitialCreate.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Open_School_Library.Data;
+
+namespace OpenSchoolLibrary.Migrations
+{
+    [DbContext(typeof(LibraryContext))]
+    [Migration("20161025194139_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Book", b =>
+                {
+                    b.Property<int>("BookId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Author");
+
+                    b.Property<int>("DeweyID");
+
+                    b.Property<int>("GenreID");
+
+                    b.Property<int>("ISBN");
+
+                    b.Property<string>("SubTitle");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("BookId");
+
+                    b.HasIndex("DeweyID");
+
+                    b.HasIndex("GenreID");
+
+                    b.ToTable("Books");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.BookLoan", b =>
+                {
+                    b.Property<int>("BookLoanID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BookID");
+
+                    b.Property<DateTime>("CheckedOutOn");
+
+                    b.Property<DateTime>("DueOn");
+
+                    b.Property<DateTime?>("ReturnedOn");
+
+                    b.Property<int?>("StudentID");
+
+                    b.HasKey("BookLoanID");
+
+                    b.HasIndex("BookID");
+
+                    b.HasIndex("StudentID");
+
+                    b.ToTable("BookLoans");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Dewey", b =>
+                {
+                    b.Property<int>("DeweyID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<float>("Number");
+
+                    b.HasKey("DeweyID");
+
+                    b.ToTable("Deweys");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Genre", b =>
+                {
+                    b.Property<int>("GenreId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("GenreId");
+
+                    b.ToTable("Genres");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Setting", b =>
+                {
+                    b.Property<int>("SettingID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("AreFinesEnabled");
+
+                    b.Property<int>("CheckoutDurationInDays");
+
+                    b.Property<decimal?>("FineAmountPerDay")
+                        .HasColumnType("Money");
+
+                    b.HasKey("SettingID");
+
+                    b.ToTable("Settings");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Student", b =>
+                {
+                    b.Property<int>("StudentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email");
+
+                    b.Property<decimal?>("Fines")
+                        .HasColumnType("Money");
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<int>("Grade");
+
+                    b.Property<int?>("IssuedID");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<int>("TeacherID");
+
+                    b.HasKey("StudentID");
+
+                    b.HasIndex("TeacherID");
+
+                    b.ToTable("Students");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Teacher", b =>
+                {
+                    b.Property<int>("TeacherID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<int>("Grade");
+
+                    b.Property<string>("LastName");
+
+                    b.HasKey("TeacherID");
+
+                    b.ToTable("Teachers");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Book", b =>
+                {
+                    b.HasOne("Open_School_Library.Data.Entities.Dewey", "Dewey")
+                        .WithMany()
+                        .HasForeignKey("DeweyID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Open_School_Library.Data.Entities.Genre", "Genre")
+                        .WithMany()
+                        .HasForeignKey("GenreID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.BookLoan", b =>
+                {
+                    b.HasOne("Open_School_Library.Data.Entities.Book", "Book")
+                        .WithMany()
+                        .HasForeignKey("BookID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Open_School_Library.Data.Entities.Student", "Student")
+                        .WithMany()
+                        .HasForeignKey("StudentID");
+                });
+
+            modelBuilder.Entity("Open_School_Library.Data.Entities.Student", b =>
+                {
+                    b.HasOne("Open_School_Library.Data.Entities.Teacher", "Teacher")
+                        .WithMany("Students")
+                        .HasForeignKey("TeacherID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ src/Open-School-Library/Migrations/20161025194139_InitialCreate.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace OpenSchoolLibrary.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deweys",
+                columns: table => new
+                {
+                    DeweyID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Number = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deweys", x => x.DeweyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    SettingID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AreFinesEnabled = table.Column<bool>(nullable: false),
+                    CheckoutDurationInDays = table.Column<int>(nullable: false),
+                    FineAmountPerDay = table.Column<decimal>(type: "Money", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.SettingID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeacherID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    Grade = table.Column<int>(nullable: false),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeacherID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(nullable: true),
+                    DeweyID = table.Column<int>(nullable: false),
+                    GenreID = table.Column<int>(nullable: false),
+                    ISBN = table.Column<int>(nullable: false),
+                    SubTitle = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                    table.ForeignKey(
+                        name: "FK_Books_Deweys_DeweyID",
+                        column: x => x.DeweyID,
+                        principalTable: "Deweys",
+                        principalColumn: "DeweyID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Books_Genres_GenreID",
+                        column: x => x.GenreID,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    Fines = table.Column<decimal>(type: "Money", nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    Grade = table.Column<int>(nullable: false),
+                    IssuedID = table.Column<int>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    TeacherID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentID);
+                    table.ForeignKey(
+                        name: "FK_Students_Teachers_TeacherID",
+                        column: x => x.TeacherID,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookLoans",
+                columns: table => new
+                {
+                    BookLoanID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BookID = table.Column<int>(nullable: false),
+                    CheckedOutOn = table.Column<DateTime>(nullable: false),
+                    DueOn = table.Column<DateTime>(nullable: false),
+                    ReturnedOn = table.Column<DateTime>(nullable: true),
+                    StudentID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookLoans", x => x.BookLoanID);
+                    table.ForeignKey(
+                        name: "FK_BookLoans_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookLoans_Students_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Students",
+                        principalColumn: "StudentID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_DeweyID",
+                table: "Books",
+                column: "DeweyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreID",
+                table: "Books",
+                column: "GenreID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookLoans_BookID",
+                table: "BookLoans",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookLoans_StudentID",
+                table: "BookLoans",
+                column: "StudentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_TeacherID",
+                table: "Students",
+                column: "TeacherID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookLoans");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Deweys");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
