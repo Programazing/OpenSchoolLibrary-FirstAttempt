8f8afc09ce9d38c2b42595af8553710840593f42
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-10-25 12:09:05 -0400

    Add basic role creation and assignment functionality.

---
+++ src/Open-School-Library/Models/RoleViewModel/RolesRegisterRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_School_Library.Models.RoleViewModel
+{
+    public class RolesRegisterRoleViewModel
+    {
+        public SelectList Roles { get; set; }
+        public SelectList Users { get; set; }
+    }
+}
