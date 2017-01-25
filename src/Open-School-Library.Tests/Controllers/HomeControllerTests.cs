5736a950c96127c8ce04245a8efee896cd8d4da2
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-01-24 23:01:41 -0500

    Add IndexReturnsAView() to HomeControllerTests class to test functionality.

--- src/Open-School-Library.Tests/Controllers/HomeControllerTests.cs
+++ src/Open-School-Library.Tests/Controllers/HomeControllerTests.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
-
+using Open_School_Library.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Open_School_Library.Tests.Controllers
 {
     public class HomeControllerTests
     {
+        [Fact]
+        public void IndexReturnsAView()
+        {
+            HomeController controller = new HomeController();
 
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.NotNull(result);
+        }
     }
 }
