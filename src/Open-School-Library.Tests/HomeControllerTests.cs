8657ac9f916a3aae13beb8baac8ef8412baed13d
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-01-24 19:50:58 -0500

    Add project Open-School-Library.Tests to solution.

---
+++ src/Open-School-Library.Tests/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Open_School_Library.Tests
+{
+    public class HomeControllerTests
+    {
+
+    }
+}
---
+++ src/Open-School-Library.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Open_School_Library.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ac4a468-df5c-4b71-bf62-49494419a92b")]
